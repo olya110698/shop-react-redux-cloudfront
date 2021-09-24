976548437576b8c76d53e62daef2457b0e78db35
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-09-24 14:51:42 +0300

    fix: change API URL for import CSV

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ybdqs8ha86.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://8i2g9e3a1b.execute-api.eu-west-1.amazonaws.com/dev", //подставить путь
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
